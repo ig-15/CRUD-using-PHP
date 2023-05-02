5139a08ad0a27b5d75aac6ee3702eab840b9fa24
Ineza Gloria <117755505+ig-15@users.noreply.github.com>
2023-05-02 11:07:26 +0200

    Update read.php

--- read.php
+++ read.php
@@ -2,38 +2,56 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Read Users</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Users</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <div class="container mt-5">
-        <?php
-        include 'connections.php';
-
-        $create='./form.html';
-        echo "<p><a href=".$create." class='btn btn-primary'>Create new user</a></p>";
-
-        $sql="SELECT id, fname, lname, email, gender FROM users"; 
-        $result= $conn->query($sql);
-        $update='./update.php';
-        $delete='./delete.php'; 
-
-        if ($result->num_rows > 0) {
-            echo "<table class='table'>";
-            echo "<thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Gender</th><th>Delete</th><th>Update</th></tr></thead><tbody>";
-            while($row = $result->fetch_assoc()) { 
-                echo "<tr><td>". $row["id"] . "</td><td>" . $row["fname"] . "</td><td>". $row["lname"] . "</td><td>" . $row["email"] . "</td><td>" . $row["gender"] . "</td><td><a href=".$delete."?id=". $row["id"] ." class='btn btn-danger'>Delete</a>". "</td><td><a href=".$update."?id=". $row["id"] ." class='btn btn-primary'>Update</a>"."</td></tr>"; 
-            }
-            echo "</tbody></table>";
-        } else {
-            echo "<p>No users</p>";
-        }
-        $conn->close();
-        ?>
+        <header class="d-flex justify-content-between my-4">
+            <h1>Users</h1>
+            <div>
+                <a href="form.html" class="btn btn-primary">New Student</a>
+                <a href="csv.php" class="btn btn-warning">Import CSV</a>
+                <a href="pdf.php" name="view" class="btn btn-info">Get PDF Document</a>
+                <a href="export.php" class="btn btn-success"><i class="fas fa-download"></i> Download CSV</a>
+            </div>
+        </header>
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Gender</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include "connections.php";
+                $sql="SELECT * FROM users";
+                $res=$conn->query($sql);
+                while($row=mysqli_fetch_array($res)){
+                ?>
+                    <tr>
+                        <td><?php echo $row["id"];?></td>
+                        <td><?php echo $row["fname"];?></td>
+                        <td><?php echo $row["lname"];?></td>
+                        <td><?php echo $row["email"];?></td>
+                        <td><?php echo $row["gender"];?></td>
+                        <td>
+                            <a href="update.php?id=<?php echo $row["id"];?>" class="btn btn-warning">Update</a>
+                            <a href="delete.php?id=<?php echo $row["id"];?>" name="delete" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                <?php
+                };
+                ?>
+            </tbody>
+        </table>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
